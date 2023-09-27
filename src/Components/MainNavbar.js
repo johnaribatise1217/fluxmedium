5cdc70f41215683b86759e693d892e65fe710902
John Aribatise <johnaribatise@hsass.org>
2023-09-27 07:46:23 +0100

    tab update

---
+++ src/Components/MainNavbar.js
@@ -0,0 +1,230 @@
+import React from 'react'
+import { alpha, makeStyles } from '@material-ui/core/styles';
+import {AppBar, Toolbar, IconButton, Typography, InputBase, Badge, MenuItem, Menu } from '@material-ui/core';
+import {FiMenu, FiMoreVertical} from 'react-icons/fi'
+import {BiSearch, BiUser} from 'react-icons/bi'
+import {GrNotification} from 'react-icons/gr'
+import {AiOutlineMail} from 'react-icons/ai'
+
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+    fontSize : "1.5rem",
+    fontFamily : "Montserrat",
+    fontWeight : 450,
+    cursor : 'pointer'
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: "#f5f5f5",
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.5),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color : '#9e9e9e'
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+}));
+
+const MainNavbar = () => {
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+    const isMenuOpen = Boolean(anchorEl);
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+    const handleProfileMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMobileMenuClose = () => {
+        setMobileMoreAnchorEl(null);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+        handleMobileMenuClose();
+    };
+
+    const handleMobileMenuOpen = (event) => {
+        setMobileMoreAnchorEl(event.currentTarget);
+    };
+
+    const menuId = 'primary-search-account-menu';
+    const renderMenu = (
+        <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        id={menuId}
+        keepMounted
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        open={isMenuOpen}
+        onClose={handleMenuClose}
+        >
+        <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+        <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+        </Menu>
+    );
+
+    const mobileMenuId = 'primary-search-account-menu-mobile';
+    const renderMobileMenu = (
+        <Menu
+        anchorEl={mobileMoreAnchorEl}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        id={mobileMenuId}
+        keepMounted
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        open={isMobileMenuOpen}
+        onClose={handleMobileMenuClose}
+        >
+        <MenuItem>
+            <IconButton aria-label="show 4 new mails" color="inherit">
+            <Badge badgeContent={4} color="secondary">
+                <AiOutlineMail style={{color : "#9e9e9e"}}/>
+            </Badge>
+            </IconButton>
+            <p>Messages</p>
+        </MenuItem>
+        <MenuItem>
+            <IconButton aria-label="show 11 new notifications" color="inherit">
+            <Badge badgeContent={11} color="secondary">
+                <GrNotification style={{color : "#9e9e9e"}}/>
+            </Badge>
+            </IconButton>
+            <p>Notifications</p>
+        </MenuItem>
+        <MenuItem onClick={handleProfileMenuOpen}>
+            <IconButton
+            aria-label="account of current user"
+            aria-controls="primary-search-account-menu"
+            aria-haspopup="true"
+            color="inherit"
+            >
+                <BiUser style={{color : "#9e9e9e"}}/>
+            </IconButton>
+            <p>Profile</p>
+        </MenuItem>
+        </Menu>
+    );
+
+    return (
+        <div className={classes.grow}>
+        <AppBar position="static" style={{background : "white" , color : "black", boxShadow :"none", borderBottom : "1px solid #e8eaf6"}}>
+            <Toolbar>
+            {/* <IconButton
+                edge="start"
+                className={classes.menuButton}
+                color="inherit"
+                aria-label="open drawer"
+            >
+                <FiMenu style={{color : "#9e9e9e"}}/>
+            </IconButton> */}
+            <Typography className={classes.title} variant="h6"  noWrap>
+                FluxMedium
+            </Typography>
+            <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                    <BiSearch style={{color : "#9e9e9e"}}/>
+                </div>
+                <InputBase
+                placeholder="Search…"
+                classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                }}
+                inputProps={{ 'aria-label': 'search' }}
+                />
+            </div>
+            <div className={classes.grow} />
+            <div className={classes.sectionDesktop}>
+                <IconButton aria-label="show 4 new mails" color="inherit">
+                <Badge badgeContent={4} color="secondary">
+                    <AiOutlineMail style={{color : "#9e9e9e", marginRight : 10}}/>
+                </Badge>
+                </IconButton>
+                <IconButton aria-label="show 17 new notifications" color="inherit">
+                <Badge badgeContent={17} color="secondary">
+                    <GrNotification style={{color : "#9e9e9e", marginRight : 10}}/>
+                </Badge>
+                </IconButton>
+                <IconButton
+                edge="end"
+                aria-label="account of current user"
+                aria-controls={menuId}
+                aria-haspopup="true"
+                onClick={handleProfileMenuOpen}
+                color="inherit"
+                >
+                    <BiUser style={{color : "#9e9e9e", marginRight : 12}}/>
+                </IconButton>
+            </div>
+            <div className={classes.sectionMobile}>
+                <IconButton
+                aria-label="show more"
+                aria-controls={mobileMenuId}
+                aria-haspopup="true"
+                onClick={handleMobileMenuOpen}
+                color="inherit"
+                >
+                    <FiMoreVertical style={{color : "#9e9e9e"}}/>
+                </IconButton>
+            </div>
+            </Toolbar>
+        </AppBar>
+        {renderMobileMenu}
+        {renderMenu}
+        </div>
+    );
+}
+
+export default MainNavbar
--- src/Pages/MediumPage/Medium.js
+++ src/Pages/MediumPage/Medium.js
@@ -1,11 +1,112 @@
 import React from 'react'
+import './medium.css'
+import MainNavbar from '../../Components/MainNavbar'
+import { Container } from '@material-ui/core'
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+
+const tabs = [
+    {
+        name : "For You",
+        id : 1,
+    },
+    {
+        name : "Javascript",
+        id : 2,
+    },
+    {
+        name : "Programming",
+        id: 3,
+    },
+    {
+        name : "Java",
+        id : 4,
+    },
+    {
+        name : "Software Development",
+        id : 5,
+    },
+    {
+        name : "Productivity",
+        id : 6,
+    }
+]
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`scrollable-auto-tabpanel-${index}`}
+      aria-labelledby={`scrollable-auto-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `scrollable-auto-tab-${index}`,
+    'aria-controls': `scrollable-auto-tabpanel-${index}`,
+  };
+}
 
 const Medium = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div>
-      
-    </div>
+    <>
+        <div className="container">
+            <MainNavbar/>
+        </div>
+        <div className="main">
+            <Container className='container'>
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    indicatorColor="primary"
+                    style={{borderBottom : "1px solid lightgray", paddingBottom : "1rem"}}
+                    textColor="primary"
+                    variant="scrollable"
+                    scrollButtons="auto"
+                    aria-label="scrollable auto tabs example"
+                >
+                    {
+                        tabs.map((tab) => (
+                            <Tab className='tabs' label={tab.name} {...a11yProps(tab.id)}/>
+                        ))
+                    }
+                </Tabs>
+            </Container>
+            <div className="right">
+            </div>
+        </div>
+    </>
   )
 }
+
+
 
 export default Medium
