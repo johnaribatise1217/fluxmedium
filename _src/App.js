38da0d8062739932247b126c9f7b88125cd5db75
Michael Piper <pipermichael@aol.com>
2023-10-18 21:00:40 +0100

    ERROR in ./src/App.js 7:0-39

---
+++ _src/App.js
@@ -0,0 +1,18 @@
+import LandingPage from "./Pages/LandingPage/LandingPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Medium from "./Pages/MediumPage/Medium";
+import Write from "./AddArticle/Write";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes >
+        <Route exact path="/" element={<LandingPage/>}/>
+        <Route path="/medium" element={<Medium/>} />
+        <Route path="/write" element={<Write/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
